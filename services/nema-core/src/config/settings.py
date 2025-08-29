a535bbdb6e90f62ee08f1a40c0443fac290de9cd
Bence Cserna <csbence@gmail.com>
2025-08-29 01:08:44 -0400

    fix: Add spare port 3003

--- services/nema-core/src/config/settings.py
+++ services/nema-core/src/config/settings.py
@@ -97,6 +97,7 @@ class Settings(BaseSettings):
                 "http://localhost:3000",
                 "http://localhost:3001", 
                 "http://localhost:3002",
+                "http://localhost:3003",
                 "http://localhost:8080",
                 "http://localhost:8081"
             ]
